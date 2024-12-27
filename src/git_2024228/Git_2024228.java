c51046e991f568cc1437bd045bb2bafa81982c7b
camila92 <75870815+camila92@users.noreply.github.com>
2024-12-27 22:05:29 +0000

    Initial commit

---
+++ src/git_2024228/Git_2024228.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2024228;
+
+/**
+ *
+ * @author camil
+ */
+public class Git_2024228 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Student name: Camila Altamira 2024228");
+       
+        // add a s simple print
+    }
+    
+}
